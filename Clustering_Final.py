e8579f88e7f85a3d3a283e40e33b9f33d9258309
nachiketmparanjape <nachiketm.paranjape@gmail.com>
2017-08-28 18:42:11 -0700

    Final

---
+++ Clustering_Final.py
@@ -0,0 +1,107 @@
+""" Clustering for creating features """
+
+#Import packages
+import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
+from sklearn.cluster import KMeans
+from sklearn import metrics
+from sklearn.decomposition import PCA
+
+
+df = pd.read_csv('clean_data_52_features.csv') #Import Data
+
+y = df.y
+df = df.drop('y',axis=1)
+
+colormap = np.array(sns.color_palette('Set1',20))
+#colormap = np.array(['k','r','g','y','b'])
+
+#Normalize the data
+def normalizer(data):
+    
+    normalized = (data-min(data))/(max(data)-min(data))
+    return normalized
+
+normdf = pd.DataFrame()
+
+for column in df:
+    normdf[column] = normalizer(df[column])
+    
+
+# K-means Clustering
+
+data = normdf.values
+# Dimensionality reduction
+pca = PCA(n_components=2)
+data2 = pca.fit_transform(data)
+
+
+sillhoutte_scores = []
+
+
+km = KMeans(n_clusters=20, algorithm='full')
+
+l = km.fit_predict(data)
+
+#    labels = km.labels_
+
+#    ss = metrics.silhouette_score(data,labels)
+#    sillhoutte_scores.append(ss)
+
+#Plot
+
+preds = km.labels_
+centroids = km.cluster_centers_
+
+
+
+
+# Plot Orginal
+fig = plt.figure(figsize=(16,6))
+
+
+plt.subplot(1, 2, 1)
+
+
+#    plt.xticks(np.arange(0,1.1,0.1))
+#    plt.yticks(np.arange(0,1.1,0.1))
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+
+x1 = list(data2[:,0])
+y1 = list(data2[:,1])
+
+plt.xlim((min(x1),max(x1)))
+plt.ylim((min(y1),max(y1)))
+
+plt.scatter(x=x1,y=y1, s=10, c=colormap[y], alpha = 0.2)
+p1 = plt.scatter([50,100], [50,100], marker='o', color=colormap[0])
+p2 = plt.scatter([50,100], [50,100], marker='o',color=colormap[1])
+p3 = plt.scatter([50,100], [50,100], marker='*',color='blue')
+plt.legend((p1,p2),('No','Yes'),numpoints=1, loc=1, fontsize=15)
+plt.title('Scatter Plot')
+ 
+# Plot Predicted with corrected values
+plt.subplot(1, 2, 2)
+
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+
+plt.scatter(x=x1, y=y1, c=colormap[preds], s=10, alpha=0.2)
+#    plt.scatter(x=centroids[:,0],y=centroids[:,1], s=100, c='blue', marker='*')
+plt.title('Clusters and Centroids')
+
+plt.savefig("./Images/20Clusters/20Cluster_" + str(i) + ".png")
+    
+
+
+print (sillhoutte_scores)
+
+#plt.plot(x=sillhoutte_scores,y=krange)
+    
+    
+    
--- Final_Prediction_and_Evaluation.py
+++ Final_Prediction_and_Evaluation.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import classification_report, roc_curve, auc, confusion_matrix, accuracy_score, recall_score
-from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, AdaBoostClassifier, GradientBoostingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
 #Import Cleaned Data
 line = "------------------------------------------------------------------\n"
 print ("\n",line)
@@ -22,8 +22,6 @@ features = df.drop('y',axis=1) #features
 print ( "There are {}% positives in the data.\n".format(round(len(df.loc[df.y == 1])*100.0/len(df),2)))
 print (line)
 
-print ("Splitting data into training and test sets...\n")
-X, Xtest, y, ytest = train_test_split(features, df['y'],random_state=1,test_size=0.3)
 
 #Define some funcitons
     
@@ -59,18 +57,65 @@ def prediction(clf,X=X,y=y,Xtest=Xtest,ytest=ytest):
         print("Area under the ROC curve = {}".format(roccurve(clf)))
     except AttributeError:
         print ("Cannot draw ROC - predict_proba is not an attribute")
+        
+def prediction_recall(clf,X=X,y=y,Xtest=Xtest,ytest=ytest):
+    """ Fits model and returns recall score """
+    
+    clf.fit(X.values, y.values)
+    preds = clf.predict(Xtest)
+    
+    return recall_score(ytest, preds)
+        
 
-# Random Forest
+#Simlating expriments to verify robustness of the model
+recall_list = []
 
-rf = RandomForestClassifier()
+for fraction in [0.2,0.4,0.6]:
+    for i in range(5):
+        
+    #Splitting Data
+    print ("Splitting data into training and test sets...\n")
+    X, Xtest, y, ytest = train_test_split(features, df['y'],random_state=1,test_size=fraction)
+    
+    
+    # Gradient Boosting Classifier
+    
+    gb = GradientBoostingClassifier()
+    
+    parameters = {'loss':['deviance', 'exponential'],
+                 'n_estimators':[20,30,50,100,200],
+                  'learning_rate':[0.1,0.5,1.0],
+                  'max_features':['sqrt','log2',None],
+                 'min_samples_split':[2,10,0.01]}
+    
+    t = time.time()
+    gs = GridSearchCV(gb,parameters,cv=5,scoring='recall',n_jobs=-1) #GridSearch
+    t = time.time()
+    print("\n %f seconds" % (time.time() - t))
+    
+    t = time.time()
+    recall = prediction_recall(gs) #Fit, Predict and Evaluate
+    recall_list.append()
+    print("\n %f seconds" % (time.time() - t))
+    
+#Print recall scores
+print ("Recall Score List = ", recall_list)
+print ("\n\n Mean Recall Score = ", sum(recall_list)/len(recall_list))
 
-parameters = {'n_estimators':[20,30,50,100,200],
+
+
+# Final Precitions
+
+gb = GradientBoostingClassifier()
+    
+parameters = {'loss':['deviance', 'exponential'],
+             'n_estimators':[20,30,50,100,200],
+              'learning_rate':[0.1,0.5,1.0],
               'max_features':['sqrt','log2',None],
-             'criterion':['gini','entropy'],
              'min_samples_split':[2,10,0.01]}
 
 t = time.time()
-gs = GridSearchCV(rf,parameters,cv=5,scoring='recall',n_jobs=-1) #GridSearch
+gs = GridSearchCV(gb,parameters,cv=5,scoring='recall',n_jobs=-1) #GridSearch
 t = time.time()
 print("\n %f seconds" % (time.time() - t))
 
